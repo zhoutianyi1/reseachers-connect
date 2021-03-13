bbd581de879ad25d4addfc96d5a387f07c8aed50
zhoutianyi1 <69812756+zhoutianyi1@users.noreply.github.com>
2021-03-13 14:56:54 -0600

    Update test_result.py

--- test_result.py
+++ test_result.py
@@ -2,12 +2,14 @@ import json
 import numpy as np
 from gensim.models import Word2Vec
 from utils import tokenize, stem, cosineSimilarity
-
+import time
 model = Word2Vec.load('model.bin')
 
 validateTitle = "Robust adaptive single neural control for a class of uncertain nonlinear systems with input nonlinearity"
+# validateTitle = "An Agent-based Architecture for Analyzing Business Processes of Real-Time Enterprises"
+# validateTitle = "ROFL: Routing on Flat Labels"
+# validateTitle = "Private Anomaly Detection Across ISP Networks"
 ignore = set("for the a an of for on in and to as or : , . ( ) ? !".split(" "))   # word to ignore when parsing research paper title
-
 processed = [stem(word.lower()) for word in tokenize(validateTitle) if word.lower() not in ignore and not word.isdigit()]
 
 def getFeatureVector(wordList):
@@ -16,7 +18,7 @@ def getFeatureVector(wordList):
     try:
       res.append(model[word])
     except:  # if the word has never been seen in the model, then append a numpy array of all zeroes
-      res.append(np.zeros(7, dtype=np.float32))  # 7 is the model word2vec features hyperparameter
+      res.append(np.zeros(7, dtype=np.float32))  # 7 is the hard-coded model word2vec features hyperparameter
   return res
 
 feature = getFeatureVector(processed)
@@ -24,26 +26,24 @@ feature = getFeatureVector(processed)
 author = ['Bryant Zhou']
 recommended = []
 
-with open('processed.json') as f:
-  paper2author = json.load(f)
-  # print(paper2author)
-  length = len(paper2author)
-  newIdx = length
-  paper2author[validateTitle] = {}
-  paper2author[validateTitle]['author'] = author
-  paper2author[validateTitle]['feature'] = feature
-  paper2author[validateTitle]['processed'] = processed
-  paper2author[validateTitle]['similarity'] = [float('inf')] * (length + 1)
-  paper2author[validateTitle]['index'] = newIdx
+start = time.time()
+with open('paper2feature.json') as f:
+  objs = json.load(f)
+  middle = time.time()
+  print(f'loading takes {middle - start} seconds')
 
-  for key in paper2author:
-    print(paper2author[key]['feature'])
-    print(f'new: {feature}')
-    sim = cosineSimilarity(paper2author[key]['feature'], feature)
-    paper2author[key]['similarity'].append(sim)
-    idx = paper2author[key]['index']
-    paper2author[validateTitle]['similarity'][idx] = sim
-    if sim < 1:
-      recommended.append((key, paper2author[key]['author']))
-  
+  for title in objs:
+    if len(objs[title]) <= 2: continue
+    try:
+      sim = cosineSimilarity(objs[title], feature)
+      # print(sim)
+    except:
+      sim = 0.1 
+      
+    if sim > 0.9:
+      # print(sim)
+      recommended.append(title)
+  end = time.time()
+  print(f'looping 100k takes {end - middle} secs')
 print(recommended)
+print(len(recommended))
