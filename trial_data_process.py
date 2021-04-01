75be1f5759054d1d1b6d4e4bf8f9e371d760adbd
zhoutianyi1 <69812756+zhoutianyi1@users.noreply.github.com>
2021-04-01 02:10:07 -0500

    Update trial_data_process.py

--- trial_data_process.py
+++ trial_data_process.py
@@ -1,25 +1,26 @@
 import json
 
 trialData = []
-with open('dblp_parser_python/dblp.json') as f:
+count = 0
+titleSet = set()
+with open('data set/dblp_parser_python/dblp.json') as f:
   for i, obj in enumerate(f):
-    if i > 10000:
+    if i > 1000000:
       break
     # convert str to object
-    toAdd = {}
+    toAdd = {count: {}}
     try: 
       convert = eval(obj)
     except:
       continue
 
-    if 'author' not in convert or 'title' not in convert: continue
-
-    toAdd["author"] = convert["author"]
-    toAdd["title"] = convert["title"]
+    if 'title' not in convert or convert['title'] == 'Home Page' or convert["title"] in titleSet: continue
+    title = convert["title"]
+    titleSet.add(title)
+    toAdd[count]['title'] = title
+    count += 1
     trialData.append(toAdd)
 
 
 with open('trial_data.json', 'w') as f:
   json.dump(trialData, f, indent = 4)
-
-  
